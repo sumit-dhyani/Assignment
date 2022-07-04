f9f1fa9f6ddd083a59cd3e0b53f3c09753494871
sumit-dhyani <dhyanisumit3@gmail.com>
2022-07-04 11:02:25 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import data from "./components/data.json";
+import React, { useEffect, useReducer, useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Students from "./components/students/Students";
+import Add from "./components/Add";
+import Update from "./components/Update";
+import Left from "./components/Left/Left";
+import Remove from "./components/Delete/Delete";
+const initialstate = { delete: false };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Delete":
+      return { delete: !state.delete, payload: action.payload };
+    case "Deleted":
+      return { delete: !state.delete };
+    default:
+      return state;
+  }
+};
+function App() {
+  const [add, setadd] = useState(false);
+  const [update, setupdate] = useState(false);
+  const [updateuser, setupdateuser] = useState("");
+  const [Data, setData] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialstate);
+  useEffect(() => {
+    setData(data);
+  }, []);
+  const Delete = (name) => {
+    dispatch({ type: "Deleted" });
+    setData(Data.filter((user) => user.name !== name));
+  };
+  return (
+    <div className="front-page">
+      <div className="section">
+        <Left />
+      </div>
+      <div className="main">
+        <div className="d-flex justify-content-between  mobile">
+          <h2>Students</h2>
+          <button
+            onClick={() => setadd(true)}
+            className="btn btn-primary align-self-end"
+          >
+            +Add
+          </button>
+        </div>
+        <div className="containing rounded-4 ">
+          <Header />
+          {Data?.length ? (
+            Data.map((user, i) => {
+              return (
+                <Students
+                  key={i}
+                  index={i + 1}
+                  name={user.name}
+                  classs={user.class}
+                  score={user.score}
+                  Delete={Delete}
+                  setupdateuser={setupdateuser}
+                  setupdate={setupdate}
+                  dispatch={dispatch}
+                />
+              );
+            })
+          ) : (
+            <>
+              <h3
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "30px"
+                }}
+              >
+                No Students left. Please add new ones using Add button or reload
+              </h3>
+            </>
+          )}
+        </div>
+        <div>
+          <h4 className="footertxt">
+            Showing {data.length} out of {data.length} entries
+          </h4>
+        </div>
+        {add ? (
+          <Add addtoggle={setadd} setData={setData} Data={Data} />
+        ) : (
+          console.log("showing main page")
+        )}
+        {update ? (
+          <Update
+            updateuser={updateuser}
+            Data={Data}
+            setData={setData}
+            setupdate={setupdate}
+          />
+        ) : (
+          console.log("toggle")
+        )}
+        {state.delete ? (
+          <Remove
+            Delete={Delete}
+            Data={Data}
+            DeleteUser={state.payload}
+            dispatch={dispatch}
+          />
+        ) : (
+          console.log("delete")
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/students/Students.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPen, faTrashCan} from '@fortawesome/free-solid-svg-icons';
+const Students = ({ index, name, classs, score, Delete,setupdateuser,setupdate,dispatch }) => {
+    const calcgrade = () => {
+        if (score > 75) {
+            return "Excellent"
+        }
+        else if (score > 30) {
+            return "Average"
+        }
+        else {
+            return "Poor"
+        }
+    }
+    const classCHange=()=>{
+        if(score>30){
+            return "result rounded-4 pass";
+        }
+        else{
+            return "result rounded-4 fail";
+        }
+        
+    }
+    const classChange2=()=>{
+        if (score > 75) {
+            return "Excellent"
+        }
+        else if (score > 30) {
+            return "Average"
+        }
+        else {
+            return "Poor"
+        }
+    }
+    return (
+        <div className='row bd d-flex align-items-center ' key={index}>
+            <div className='col-2 col-sm-2  col-md-1'>{index}</div>
+            <div className='col-2 col-sm-2 col-md-3'>{name}</div>
+            <div className='col-2 col-sm-2 col-md-2'>{classs}th</div>
+            <div className='col-2 col-sm-2 col-md-2 '>
+                <div className='d-flex justify-content-start text-center align-items-center'><a className={classCHange()}>{score > 30 ? "Passed" : "Failed"}</a></div></div>
+            <div className='col-2 col-sm-2 col-md-2'>{score}/100</div>
+            <div className='col-2 col-sm-2 col-md-2 d-flex update'>
+                <div className={classChange2()}>{
+                    calcgrade()
+
+                }</div>
+                <div className='icons'>
+                <a onClick={()=>{setupdateuser(name); setupdate(true)}} href='#'><FontAwesomeIcon icon={faPen} /></a>
+                <a onClick={()=>dispatch({type:"Delete",payload:name})} href='#'><FontAwesomeIcon icon={faTrashCan} /></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Students;
